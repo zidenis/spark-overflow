5f6d6b28f0a4a2b74bc24db8e1b0260eb112e8fa
zidenis <denisalbuquerque@gmail.com>
2019-04-10 09:31:48 -0300

    scripts to run spark jobs on cluster

---
+++ run-ldaanalysis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+spark-submit --class br.ufrn.dimap.forall.spark.LDADataAnalysis --conf "spark.cores.max=24" --conf "spark.executor.cores=4" --conf "spark.executor.memory=3g" --conf "spark.driver.memory=6g" --conf "spark.driver.cores=8" target/scala-2.11/SparkOverflow-assembly-1.0.jar > logs/output.log.`date +%y%m%d_%H%M` 2> logs/error.log
